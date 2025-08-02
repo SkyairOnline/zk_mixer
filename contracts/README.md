c904bce506c6f2e57a6c5093f16ff468843c41a7
skyaironline <aldo@batamon.asia>
2025-08-02 15:07:55 +0700

    Create mixer smart contract

---
+++ contracts/README.md
@@ -0,0 +1,10 @@
+# ZK Mixer Project
+
+- Deposit: user can deposit ETH into the mizer to break the connection between depositor and withdrawer.
+- Withdraw: users will withdraw using a ZK proof (Noir - generated off-chain) of knowledge of their deposit.
+- We will only allow users to deposit a fixed amount of ETH (0.001 ETH)
+
+## Proof
+- we need to check that the commitment is present in the Merkle tree
+    - proposed root
+    - Merkle tree
---
+++ contracts/src/Mixer.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract Mixer {
+    IVerifier public immutable i_verifier;
+
+    // mapping to store whether a commitment has been used
+    mapping(bytes32 => bool) public s_commitments;
+
+    // the denomination of the mixer, i.e. the amount of ETH that can be deposited
+    uint256 public constant DENOMINATION = 0.001 ether;
+
+    error Mixer__CommitmentAlreadyAdded(bytes32 commitment);
+    error Mixer__DepositAmountNotCorrect(uint256 amountSent, uint256 expectedAmount);
+
+    constructor(IVerifier _verifier) {
+        i_verifier = _verifier;
+    }
+
+    // @notice Deposit funds into the mixer
+    // @param _commitment the poseiden commitment of the nullifier and secret (generated off-chain)
+    function deposit(bytes32 _commitment) external payable {
+        // check whether the commitment has already been used so we can prevent a deposit being added twice
+        if (s_commitments[_commitment]) {
+            revert Mixer__CommitmentAlreadyAdded(_commitment);
+        }
+        // check that the amount of ETH send it the correct denomination
+        if(msg.value != DENOMINATION) {
+            revert Mixer__DepositAmountNotCorrect(msg.value, DENOMINATION);
+        }
+        // allow the user to send ETH and make sure it is of the correct fixed amount (denomination)
+        // add the commitment to a data structure containing all of the commitments
+        s_commitments[_commitment] = true;
+    }
+
+    // @notice Withdraw funds from the mixer in a private way
+    // @param _proof proof that the user has the right to withdraw (they know a valid commitment)
+    function withdraw(bytes32 _proof) external {
+        // check that the proof is valid by calling the verifier contract
+        // check that the nullifier has not been used before (to prevent double spending)
+        // transfer the funds to the user
+    }
+}
